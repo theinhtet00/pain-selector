315d59b46fddc33f236ba1dc2d0c08666cf82b5d
Thein Htet Oo <bitinyourheart@gmail.com>
2024-10-02 17:00:52 +0700

    change button name

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -10,7 +10,7 @@ const Home = () => {
           className="bg-blue-600 border-2 w-full md:w-52 text-center border-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-md"
           to={NavigationScreen.Abdomen}
         >
-          Abdominal Selection
+          Abdomen Selection
         </Link>
         <Link
           className="border-2 mt-3 md:mt-0 w-full md:w-52 text-center text-blue-600 font-bold hover:text-white hover:bg-blue-600 border-blue-600 px-4 py-2 rounded-md"
