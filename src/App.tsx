1a7094734c6fc8bb44330d94e25d56b1fe3ca83e
Thein Htet Oo <bitinyourheart@gmail.com>
2024-10-02 17:12:51 +0700

    change dynamic route name & vercel config

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,16 @@ import AbdomenSelector from './components/AbdomenSelector';
 import FingerSelector from './components/FingerSelector';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './components/Home';
+import { NavigationScreen } from './model/routes';
 
 const App: React.FC = () => {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100 p-4">
       <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/abdominal' element={<AbdomenSelector/>}/>
-        <Route path='/finger' element={<FingerSelector/>}/>
+        <Route path={NavigationScreen.Home} element={<Home/>}/>
+        <Route path={NavigationScreen.Abdomen} element={<AbdomenSelector/>}/>
+        <Route path={NavigationScreen.Finger} element={<FingerSelector/>}/>
       </Routes>
       </BrowserRouter>
       
